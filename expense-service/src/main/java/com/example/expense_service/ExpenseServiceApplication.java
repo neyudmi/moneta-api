d165957c0134bf307d8e88f7b3569e3b6f7b14d0
neyudmi <itsduyencm@gmail.com>
2026-09-21 21:44:37 +0700

    feat: add Swagger OpenAPI documentation

--- expense-service/src/main/java/com/example/expense_service/ExpenseServiceApplication.java
+++ expense-service/src/main/java/com/example/expense_service/ExpenseServiceApplication.java
@@ -2,10 +2,8 @@ package com.example.expense_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class ExpenseServiceApplication {
 
 	public static void main(String[] args) {
